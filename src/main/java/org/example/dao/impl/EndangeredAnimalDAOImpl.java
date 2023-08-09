7ce3e4b3a958bc1bb11c7ea6fba7ab6d6cb55cb9
Glorine Vihendah <92040630+gvihendah@users.noreply.github.com>
2023-08-10 02:09:29 +0300

    changes made to the project

---
+++ src/main/java/org/example/dao/impl/EndangeredAnimalDAOImpl.java
@@ -0,0 +1,63 @@
+package org.example.dao.impl;
+
+public class EndangeredAnimalDAOImpl implements EndangeredAnimalDAO {
+    private final Sql2o sql2o;
+
+    public EndangeredAnimalDAOImpl(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public void create(EndangeredAnimal endangeredAnimal) {
+        String query = "INSERT INTO endangered_animals (animal_id, health, age) VALUES (:animalId, :health, :age)";
+        try (Connection con = sql2o.open()) {
+            con.createQuery(query, false)
+                    .bind(endangeredAnimal)
+                    .executeUpdate();
+        } catch (Sql2oException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    @Override
+    public void update(EndangeredAnimal endangeredAnimal) {
+        String sql = "UPDATE endangered_animals SET (animal_id, health, age) = (:animalId, :health, :age) WHERE id=:id";
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql, false)
+                    .bind(endangeredAnimal)
+                    .executeUpdate();
+        } catch (Sql2oException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    @Override
+    public List<EndangeredAnimal> getAll() {
+        try (Connection con = sql2o.open()) {
+            return con.createQuery("SELECT * FROM endangered_animals")
+                    .addColumnMapping("animal_id", "animalId")
+                    .executeAndFetch(EndangeredAnimal.class);
+        }
+    }
+
+    @Override
+    public EndangeredAnimal getById(Integer id) {
+        try (Connection con = sql2o.open()) {
+            return con.createQuery("SELECT * FROM endangered_animals WHERE id = :id")
+                    .addParameter("id", id)
+                    .addColumnMapping("animal_id", "animalId")
+                    .executeAndFetchFirst(EndangeredAnimal.class);
+        }    }
+
+    @Override
+    public void delete(String id) {
+        String sql = "DELETE from endangered_animals WHERE id=:id";
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        } catch (Sql2oException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
