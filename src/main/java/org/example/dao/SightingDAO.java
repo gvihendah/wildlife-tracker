ce1c023c7bdc9e80b056aa0b9fcf77e21d11de6a
Glorine Vihendah <92040630+gvihendah@users.noreply.github.com>
2023-08-10 02:00:02 +0300

    files added to the project

---
+++ src/main/java/org/example/dao/SightingDAO.java
@@ -0,0 +1,12 @@
+package org.example.dao;
+
+import org.example.models.Sighting;
+import java.util.List;
+
+public class SightingDAO {
+    void create(Sighting sighting);
+    void update(Sighting sighting);
+    List<Sighting> getAll();
+    Sighting getById(Integer id);
+    void delete(String id);
+}
